f533ca082719708f6d15067a6f557bd123981538
Yago Afonso <yago.afonso@hotmail.com>
2022-09-17 18:04:09 -0400

    Update: add keen-slider

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,13 @@
 import { useState, useEffect } from 'react';
+import { useKeenSlider } from "keen-slider/react"
+import "keen-slider/keen-slider.min.css";
+import * as Dialog from '@radix-ui/react-dialog';
+import axios  from 'axios';
+
 import { CreateAdBanner } from './components/CreateAdBanner';
 import { CreateAdModal } from './components/CreateAdModal';
 import { GameBanner } from './components/GameBanner';
-import * as Dialog from '@radix-ui/react-dialog';
-import axios  from 'axios';
+
 
 import './styles/main.css';
 
@@ -22,32 +26,54 @@ interface Game {
 function App() {
   const [games, setGames] = useState<Game[]>([])
 
+  const animation = { duration: 20000, easing: (t: number) => t };
+
   useEffect(() => {
     axios('http://localhost:3333/games').then(response => {
       setGames(response.data)
     })
   }, [])
 
+  const [sliderRef] = useKeenSlider<HTMLDivElement>({
+    loop: true,
+    renderMode: "performance",
+    drag: false,
+    slides: {
+      perView: 5,
+      origin: 'auto',
+      spacing: 20,
+    },
+    created(s) {
+      s.moveToIdx(5, true, animation);
+    },
+    updated(s) {
+      s.moveToIdx(s.track.details.abs + 5, true, animation);
+    },
+    animationEnded(s) {
+      s.moveToIdx(s.track.details.abs + 5, true, animation);
+    }
+  });
+
+
   return (
-   <div className="max-w-[1334px] mx-auto flex flex-col items-center my-20">
+   <div className="max-w-[1280px] mx-auto flex flex-col items-center justify-center my-20">
     <img src={logoImg} alt="" />
-      <h1 className='text-6xl text-white font-black mt-20'>
-        Seu <span className='text-transparent bg-nlw-gradient bg-clip-text'>duo</span> está aqui.
+      <h1 className="text-6xl text-white font-black mt-20">
+        Seu <span className="text-transparent bg-nlw-gradient bg-clip-text">duo</span> está aqui.
       </h1>
 
-      <div className='grid grid-cols-6 gap-6 mt-16'>
+      <div ref={sliderRef} className="keen-slider max-w-[1280px]flex mt-16">
         {games.map(game => {
           return (
             <GameBanner
               key={game.id} 
               title={game.title}
               bannerUrl={game.bannerUrl}
-              adsCount={game._count.ads}/>
+              adsCount={game._count.ads}
+            />
           );
         })}
         
-
-
       </div>
 
       <Dialog.Root>
--- web/src/components/GameBanner.tsx
+++ web/src/components/GameBanner.tsx
@@ -1,3 +1,5 @@
+import { useKeenSlider } from "keen-slider/react"
+
 interface GameBannerProps {
   bannerUrl: string;
   title: string;
@@ -6,11 +8,11 @@ interface GameBannerProps {
 
 export function GameBanner(props: GameBannerProps) {
   return(
-    <a href="" className='relative rounded-lg overflow-hidden'>
+    <a href="" className="keen-slider__slide relative rounded-lg overflow-hidden flex flex-col center">
       <img src={props.bannerUrl} alt="" />
-      <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0 '>
-        <strong className='font-bold text-white block'>{props.title}</strong>
-        <span className='text-zinc-300 text-sm block'>{props.adsCount} anúncio(s)</span>
+      <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0 ">
+        <strong className="font-bold text-white block">{props.title}</strong>
+        <span className="text-zinc-300 text-sm block">{props.adsCount} anúncio(s)</span>
       </div>
     </a>
   );

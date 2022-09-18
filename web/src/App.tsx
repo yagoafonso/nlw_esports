262131a60062f9a63ca4272d3c51f37aa5d3a2d7
Yago Afonso <yago.afonso@hotmail.com>
2022-09-18 16:47:22 -0400

    Update: add responsiveness

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -53,37 +53,43 @@ function App() {
       "(min-width: 425px)": {
         slides: {
           perView: 2,
-          spacing: 18,
+          spacing: 10,
         },
       },
       "(min-width: 640px)": {
         slides: {
           perView: 3,
-          spacing: 16,
+          spacing: 12,
         },
       },
       "(min-width: 768px)": {
         slides: {
           perView: 4,
-          spacing: 18,
+          spacing: 14,
         },
       },
       "(min-width: 1024px)": {
         slides: {
           perView: 5,
-          spacing: 20,
+          spacing: 16,
         },
       },
       "(min-width: 1280px)": {
         slides:{
           perView: 6,
-          spacing: 24,
+          spacing: 18,
+        },
+      },
+      "(min-width: 1536px)": {
+        slides:{
+          perView: 7,
+          spacing: 20,
         },
       },
     },
   })
 
-  function Arrow(props: {disabled: boolean, left?: boolean, onClick: (event: any) => void }){
+  function Arrow(props: {disabled: boolean, left?: boolean, onClick: (event: any) => void}){
     const disabeld = props.disabled ? " arrow--disabled" : ""
     return(
         <svg
@@ -108,9 +114,9 @@ function App() {
 
 
   return (
-   <div className="max-w-[1334px] mx-auto flex flex-col items-center justify-center my-20">
+   <div className="max-w-[1334px] mx-auto flex flex-col items-center justify-center my-20 sm:my-14 md:my-16 xl:my-20">
     <img src={logoImg} alt="Logotipo da página Esports NLW" />
-      <h1 className="text-6xl text-white font-black mt-20">
+      <h1 className="mt-10 text-xl font-black text-white sm:mt-4 md:mt-16 md:text-4xl lg:text-5xl xl:mt-20 xl:text:text-6xl">
         Seu <span className="text-transparent bg-nlw-gradient bg-clip-text">duo</span> está aqui.
       </h1>
 
--- web/src/components/CreateAdBanner.tsx
+++ web/src/components/CreateAdBanner.tsx
@@ -6,11 +6,11 @@ export function CreateAdBanner() {
     <div className='pt-1 bg-nlw-gradient mt-8 self-stretch rounded-lg overflow-hidden'>
     <div className='bg-[#2A2634] px-8 py-6 self-stretch rounded-lg flex justify-between items-center'>
       <div>
-        <strong className='text-2xl text-white font-black block'>Não encontrou seu duo?</strong>
-        <span className='text-zinc-400 block'>Publique um anúncio para encontrar novos players!</span>
+        <strong className='text-lg text-white font-black block sm:text-x1 md:text-2xl'>Não encontrou seu duo?</strong>
+        <span className='text-sm text-zinc-400 block md:text-base'>Publique um anúncio para encontrar novos players!</span>
       </div>
 
-      <Dialog.Trigger className='py-3 px-4 bg-violet-500 hover:bg-violet-700 text-white rounded flex items-center gap-3'>
+      <Dialog.Trigger className='py-3 px-4 bg-violet-500 hover:bg-violet-700 text-white rounded flex items-center gap-3sm:py-2 sm:px-3 sm:text-base md:py-3 md:px-4'>
         <MagnifyingGlassPlus size={24}/>
         Publicar anúncio
       </Dialog.Trigger>

82ce0cd684b2f2e351715a860dd009a2ca21ed60
Yago Garcia <yago.afonso@hotmail.com>
2022-09-19 11:33:00 -0400

    Update: change port server

--- server/src/server.ts
+++ server/src/server.ts
@@ -92,4 +92,6 @@ app.get('/ads/:id/discord', async(request, response) =>{
     discord: ad.discord,
   })
 })
-app.listen(3333)
+app.listen(process.env.PORT || 3333,()=>{
+  console.log("Servidor on!")
+})
